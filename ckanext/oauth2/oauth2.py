8df631b669daa2c464a1cff1b644786d31d5d92a
sagargg <info@sagarg.com.np>
2021-05-12 11:27:13 +0545

    remove brackets and comma from the username

--- ckanext/oauth2/oauth2.py
+++ ckanext/oauth2/oauth2.py
@@ -24,6 +24,7 @@ from __future__ import unicode_literals
 import base64
 import ckan.model as model
 import db
+import re
 import json
 import logging
 from six.moves.urllib.parse import urljoin, urlparse, urlunparse
@@ -172,7 +173,8 @@ class OAuth2Helper(object):
 
     def user_json(self, user_data):
         email = user_data[self.profile_api_mail_field]
-        user_name = user_data[self.profile_api_user_field].lower().replace(" ", "_")
+        user_name = re.sub(r"\((.*?.)\), +", "", user_data[self.profile_api_user_field]).lower().replace(" ", "_")
+
 
         # In CKAN can exists more than one user associated with the same email
         # Some providers, like Google and FIWARE only allows one account per email
